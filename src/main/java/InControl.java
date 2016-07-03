a1c5ecb9b9e20374982fc25413164c5051f1f581
Asel Petrova <asel.petrova@gmail.com>
2016-07-03 23:39:17 +0300

    without headers

--- src/main/java/InControl.java
+++ src/main/java/InControl.java
@@ -5,11 +5,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Асюша on 26.06.2016.
@@ -27,6 +29,7 @@ public class InControl {
             XSSFWorkbook wbKPP = new XSSFWorkbook(fileKPP);
             XSSFSheet sheetKPP = wbKPP.getSheetAt(0);
 
+            int indexRow = 0;
             for (int indexkpp = 2; indexkpp < sheetKPP.getLastRowNum(); ++indexkpp) {
                 Row rowKPP = sheetKPP.getRow(indexkpp);
 
@@ -36,6 +39,7 @@ public class InControl {
                 Cell colT = rowKPP.getCell(8);
 
                 XSSFSheet sheetList = wbList.getSheetAt(0);
+
                 for (int indexrow = 1; indexrow < sheetList.getLastRowNum(); ++indexrow) {
 
                     Row row = sheetList.getRow(indexrow);
@@ -49,34 +53,68 @@ public class InControl {
                     String cellFIO = colFIO.getStringCellValue();
                     System.out.println(cellFIO);
 
-                    Integer mustTime = Integer.valueOf(colTime.getStringCellValue().replaceAll(":", ""));
-
+                    Integer mustTime = tomin(colTime.getStringCellValue());
+                    System.out.println(mustTime);
 
                     if (colFIO.getStringCellValue().replaceAll(" ", "").toUpperCase().equals(
-                            colF.getStringCellValue() +
+                            (colF.getStringCellValue() +
                                     colI.getStringCellValue() +
-                                    colO.getStringCellValue().toUpperCase())) {
+                                    colO.getStringCellValue()).toUpperCase())) {
 
 
-                        XSSFSheet monthSheet = wbList.getSheet(getSheetName(path));
+                        String sheetName = getSheetName(path);
+                        XSSFSheet monthSheet = wbList.getSheet(sheetName);
                         if (monthSheet == null)
-                            monthSheet = wbList.createSheet(getSheetName(path));
+                            monthSheet = wbList.createSheet(sheetName);
 
-                        Row monthRow = monthSheet.createRow(monthSheet.getLastRowNum());
+
+                        Row monthRow = getExistRow(monthSheet, colFIO.getStringCellValue(), indexRow++);
+
                         Cell monthCellFIO = monthRow.createCell(0);
                         monthCellFIO.setCellValue(colFIO.getStringCellValue());
+
                         Cell monthCellTime = monthRow.createCell(1);
+                        monthCellTime.setCellValue(colTime.getStringCellValue());
                         Cell monthCellDif = monthRow.createCell(getKPPDay(path) + 1);
+                        Integer difTime = null;
+                        try {
+                            difTime = tomin(colT.getStringCellValue()) - mustTime;
+                        } catch (NumberFormatException e) {
+                            break;
 
+                        }
+                        monthCellDif.setCellValue(difTime < 0 ? 0 : difTime);
+                        break;
                     }
-
                 }
+            }
+        }
+        fileList.close();
+        FileOutputStream out = new FileOutputStream(new File("List.xlsx"));
+        wbList.write(out);
+        out.close();
+    }
 
+    private static Row getExistRow(XSSFSheet monthSheet, String findFIO, int indexRow) {
+        for (int indexrow = 0; indexrow < monthSheet.getLastRowNum(); ++indexrow) {
+            Row row = monthSheet.getRow(indexrow);
 
+            Cell colFIO = row.getCell(0);
+
+            if (findFIO.equals(colFIO.getStringCellValue())) {
+                return row;
             }
 
-
         }
+        return monthSheet.createRow(indexRow);
+    }
+
+
+    private static Integer tomin(String timeValue) {
+
+        String[] arrayTime = timeValue.split(":");
+        return Integer.valueOf(arrayTime[0]) * 60 + Integer.valueOf(arrayTime[1]);
+
     }
 
     private static String getSheetName(Path path) {
@@ -100,8 +138,8 @@ public class InControl {
 
     private static int getKPPDay(Path path) {
         String fileName = path.toFile().getName();
-        System.out.println(fileName.substring(fileName.length() - 13, fileName.length() - 10));
-        return Integer.valueOf(fileName.substring(fileName.length() - 13, fileName.length() - 10));
+        System.out.println(fileName.substring(fileName.length() - 13, fileName.length() - 11));
+        return Integer.valueOf(fileName.substring(fileName.length() - 13, fileName.length() - 11));
 
     }
 }
